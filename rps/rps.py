691053f8b8d39c9ade73a383deae7059bbb2999a
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-08-26 12:01:33 +0100

    Add returns to each start function call

--- rps/rps.py
+++ rps/rps.py
@@ -9,7 +9,7 @@ def start():
 
   if(playerChoice not in possibleChoices):
     print('You have entered an invalid string, please try again!\n')
-    start()
+    return start()
   else:
     print('I\'m thinking...')
     time.sleep(2)
@@ -25,7 +25,7 @@ def compareChoices(choice1, choice2):
   print('You chose', choice1, 'and I chose', choice2)
   if(choice1 == choice2):
     print('It was a draw! Try again!\n')
-    start()
+    return start()
   else:
     result = (possibleChoices.index(choice2) + 1) % 3 == possibleChoices.index(choice1)
     result = 'win' if result else 'lose'
