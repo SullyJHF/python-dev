8085976e808332d7457eb2a45f1f497a804bdd86
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-08-26 12:01:52 +0100

    Add lizard and spock

---
+++ rps/rpsls.py
@@ -0,0 +1,35 @@
+import time
+import random
+
+possibleChoices = ['rock', 'lizard', 'spock', 'scissors', 'paper']
+
+def start():
+  print('Are you ready to take on the rock paper scissors lizard spock challenge?')
+  playerChoice = input('Input your choice here: ')
+
+  if(playerChoice not in possibleChoices):
+    print('You have entered an invalid string, please try again!\n')
+    return start()
+  else:
+    print('I\'m thinking...')
+    time.sleep(2)
+    cpuChoice = cpuChoose()
+    print()
+    compareChoices(playerChoice, cpuChoice)
+
+def cpuChoose():
+  cpuChoice = random.choice(possibleChoices)
+  return cpuChoice
+
+def compareChoices(choice1, choice2):
+  print('You chose', choice1, 'and I chose', choice2)
+  if(choice1 == choice2):
+    print('It was a draw! Try again!\n')
+    return start()
+  else:
+    result = (possibleChoices.index(choice1) + 1) % 5 == possibleChoices.index(choice2)
+    result = result if result else (possibleChoices.index(choice1) - 2) % 5 == possibleChoices.index(choice2)
+    result = 'win' if result else 'lose'
+    print('You', result + '!')
+
+start()
