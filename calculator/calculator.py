f30f2ececf2434c124fb4501e0e21ec36428e61d
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-09-30 11:37:02 +0100

    Include calculator.py

---
+++ calculator/calculator.py
@@ -0,0 +1,11 @@
+def sum (numbers):
+    total = 0;
+    for num in numbers:
+        total += num
+    return total
+
+def multiply (numbers):
+    total = 1
+    for num in numbers:
+        total *= num
+    return total
